5f5b0b3ba88922820fee43961ba5796782dcd972
Bui  Duong <duongbd0512@gmail.com>
2021-04-15 22:32:55 +0700

    Sap xep tham lam

---
+++ GreedyArrangementCppToWinform/GreedyArrangementCppToWinform/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreedyArrangementCppToWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] A = txbValueArray1.Text.Split(',');
+            int[] a = Array.ConvertAll(A, s => int.Parse(s));
+            string[] B = txbValueArray1.Text.Split(',');
+            int[] b = Array.ConvertAll(B, s => int.Parse(s));
+            GreedyArrangement greedyArrangement = new GreedyArrangement();
+            if (greedyArrangement.GreddyArray(a, b) == a.Length)
+            {
+                MessageBox.Show("Mảng có thể sắp xếp bằng cách đảo ngược");
+            }
+            else
+            {
+                MessageBox.Show("Mảng không thể sắp xếp bằng cách đảo ngược");
+            }
+        }
+    }
+}
---
+++ GreedyArrangementCppToWinform/GreedyArrangementCppToWinform/GreedyProgram.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GreedyArrangementCppToWinform
+{
+    class GreedyArrangement
+    {
+
+        #region Hàm sắp xếp mảng bằng Quick Sort :(((
+        public static void Quick_Sort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    Quick_Sort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    Quick_Sort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        public static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+        #endregion
+        public int GreddyArray(int[] a, int[] b)
+        {
+            //Console.OutputEncoding = Encoding.UTF8;
+            int n = a.Length;
+            //int[] a = new int[n];
+            //int[] b = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    a[i] = Int32.Parse(Console.ReadLine());
+            //    b[i] = a[i];
+            //}
+            Quick_Sort(a, 0, n - 1);
+            int check = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] == b[i] || a[i] == b[n - i - 1])
+                {
+                    check++;
+                }
+            }
+            return check;
+        }
+    }
+}
