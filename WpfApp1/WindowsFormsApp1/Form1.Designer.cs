9ae564fa8d85d72d2f650dffc204df4f406dbe22
Bui  Duong <duongbd0512@gmail.com>
2021-04-22 22:35:14 +0700

    commit

---
+++ WpfApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbClass = new System.Windows.Forms.ComboBox();
+            this.cbStudent = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbAge = new System.Windows.Forms.ComboBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbClass
+            // 
+            this.cbClass.FormattingEnabled = true;
+            this.cbClass.Location = new System.Drawing.Point(151, 110);
+            this.cbClass.Name = "cbClass";
+            this.cbClass.Size = new System.Drawing.Size(121, 23);
+            this.cbClass.TabIndex = 0;
+            // 
+            // cbStudent
+            // 
+            this.cbStudent.FormattingEnabled = true;
+            this.cbStudent.Location = new System.Drawing.Point(335, 110);
+            this.cbStudent.Name = "cbStudent";
+            this.cbStudent.Size = new System.Drawing.Size(121, 23);
+            this.cbStudent.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 244);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // cbAge
+            // 
+            this.cbAge.FormattingEnabled = true;
+            this.cbAge.Location = new System.Drawing.Point(514, 110);
+            this.cbAge.Name = "cbAge";
+            this.cbAge.Size = new System.Drawing.Size(121, 23);
+            this.cbAge.TabIndex = 4;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(151, 40);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 5;
+            this.btnClick.Text = "Search";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.cbAge);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbStudent);
+            this.Controls.Add(this.cbClass);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbClass;
+        private System.Windows.Forms.ComboBox cbStudent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cbAge;
+        private System.Windows.Forms.Button btnClick;
+    }
+}
+
